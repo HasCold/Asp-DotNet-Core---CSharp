938d3ed53121f8b46f8af825347e4dd3bd926e5a
Hasan <ha03330224926@gmail.com>
2026-07-19 13:12:09 -0700

    initial commit

---
+++ c_sharp-learning/MathUtilities.cs
@@ -0,0 +1,7 @@
+namespace MathUtilities;
+
+public class MathUtilities {
+    public static int Addition(int arg1, int arg2) {
+        return arg1 + arg2;
+    }
+}
---
+++ c_sharp-learning/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp;
+
+using System;
+using MathUtilities;
+
+class Program{
+    static void Main(string[] args){
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Hello, C# Programming"); 
+        
+        Console.WriteLine($"The addtion result => {MathUtilities.Addition(10, 20)}");
+    }
+}
