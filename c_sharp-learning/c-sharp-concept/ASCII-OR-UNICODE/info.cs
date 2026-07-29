1c5449ebad7d261821e0aa4aa26ebe2a56c2b3c6
Hasan <ha03330224926@gmail.com>
2026-07-29 13:42:44 -0700

    added loops

--- c_sharp-learning/c-sharp-concept/ASCII-OR-UNICODE/info.cs
+++ c_sharp-learning/c-sharp-concept/ASCII-OR-UNICODE/info.cs
@@ -42,8 +42,75 @@ internal class ASCIIORUNICODE
         //     Console.WriteLine(i);
         // }
 
-        for(int i = 1; i <= 10; i++){
-            Console.WriteLine(i);
+        // int ch = 'A';
+        // Console.WriteLine((char)ch + 32);
+
+        // 1 2 3 4 5
+        //   1 2 3 4 
+        //     1 2 3  
+        //       1 2   
+        //         1   
+
+        for (int i = 5; i >= 1; i--)
+        {
+            for (int s = 5; s > i; s--)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+            Console.WriteLine();
         }
+
+        char ch;
+        bool flag = true;
+
+        while (flag)
+        {
+            Console.WriteLine("Press C for continue \n Press E for exit");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            switch (ch)
+            {
+                case 'C':
+                    Console.WriteLine("Loop will Continue");
+                    break;
+
+                case 'E':
+                    flag = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong choice Try Again !!");
+                    break;
+            }
+        }
+
+        bool flag2 = false;
+        char ch2;
+        do
+        {
+            Console.WriteLine("Press C for continue \n Press E for exit");
+            ch2 = Convert.ToChar(Console.ReadLine());
+
+            switch (ch2)
+            {
+                case 'C':
+                    Console.WriteLine("Loop will Continue");
+                    break;
+
+                case 'E':
+                    flag2 = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong choice Try Again !!");
+                    break;
+            }
+        } while (flag2);
+
     }
 }
